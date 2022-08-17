2b8121fdadf8d11bbdae74e63c01bf09d8d3e9e4
EstivenCano <estivencano99@gmail.com>
2022-08-16 20:15:49 -0500

    [Add] Initial config

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+      <Head>
+        <title>Handy POS</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main className='flex w-full flex-1 flex-col items-center justify-center px-20 text-center'>
+        <h1 className='text-6xl font-bold'>
+          Welcome to{" "}
+          <a className='text-blue-600' href='https://nextjs.org'>
+            Handy POS!
+          </a>
+        </h1>
+      </main>
+
+      <footer className='flex h-24 w-full items-center justify-center border-t'>
+        <a
+          className='flex items-center justify-center gap-2'
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'>
+          Powered by <strong>TailSoft</strong>
+        </a>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
