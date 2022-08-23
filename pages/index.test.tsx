02072d3b4466ca80eb7f9a7b3536067f2016db3b
EstivenCano <estivencano99@gmail.com>
2022-08-23 17:58:30 -0500

    [Add] Index test

---
+++ pages/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Home from './index';
+
+describe(`it should render info correctly`, () => {
+  render(<Home />);
+
+  it(`should render main message`, () => {
+    expect(screen.getByRole(`link`, { name: `Handy POS!` })).toBeVisible();
+  });
+});
