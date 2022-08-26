88a41cabcdf7e45729fcde36ef8b729217e94013
EstivenCano <estivencano99@gmail.com>
2022-08-25 23:26:19 -0500

    [Add] Custom document page

---
+++ pages/_document.tsx
@@ -0,0 +1,11 @@
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    return await Document.getInitialProps(ctx);
+  }
+}
+
+export default MyDocument;
