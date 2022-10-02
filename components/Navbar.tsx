d0856e7609fec4785bd8b911ab1877cd1f716c24
EstivenCano <estivencano99@gmail.com>
2022-10-01 22:53:51 -0500

    [Add] Default Navbar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,20 +2,24 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export const Navbar = () => {
+  const links = [
+    { path: '/', name: 'Home' },
+    { path: '/', name: 'About' },
+    { path: '/', name: 'Services' },
+    { path: '/', name: 'Pricing' },
+    { path: '/', name: 'Contact' },
+  ];
+
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 rounded dark:bg-gray-900 w-full py-2">
+    <nav className="px-2 sm:px-4 rounded w-full py-2">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <Image
-            src="/images/favicon.ico"
-            width={50}
-            height={50}
-            alt="handy-bill-logo"
-          />
-          <span className="self-center text-xl pl-2 font-semibold whitespace-nowrap dark:text-white">
-            Handy Bill
-          </span>
-        </a>
+        <Image
+          src="/images/favicon.ico"
+          width={50}
+          height={50}
+          alt="handy-bill-logo"
+        />
+        <h4 className="font-bold whitespace-nowrap">Handy Bill</h4>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -39,48 +43,17 @@ export const Navbar = () => {
           </svg>
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-                aria-current="page"
+          <ul className="flex flex-col p-4 mt-4 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+            {links.map((item) => (
+              <li
+                key={item.name}
+                className="nav-item font-bold hover:scale-105"
               >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/"
-                className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </Link>
-            </li>
+                <Link href={item.path} aria-current="page">
+                  {item.name}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
