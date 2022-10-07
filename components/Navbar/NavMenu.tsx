939e5e6e40bedcafc772b0a3b96734dfe94809a6
EstivenCano <estivencano99@gmail.com>
2022-10-06 20:35:41 -0500

    [Add] useComponentVisible hook

--- components/Navbar/NavMenu.tsx
+++ components/Navbar/NavMenu.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link';
-import { useState } from 'react';
 
+import { useComponentVisible } from '../../hooks/useComponentVisible';
 import links from './links.json';
 
 export const NavMenu = () => {
-  const [open, setOpen] = useState(false);
+  const { ref, isComponentVisible, setIsComponentVisible } =
+    useComponentVisible(false);
   return (
     <>
       <button
         data-collapse-toggle="navbar-default"
         type="button"
-        onClick={() => setOpen((prevState) => !prevState)}
-        onMouseOutCapture={() => setOpen((prevState) => prevState || true)}
-        className="p-2 rounded-lg md:hidden focus:ring-2 hover:bg-foreground focus:ring-primary-700 text-gray-400"
+        onClick={() => setIsComponentVisible((prevState) => !prevState)}
+        className="p-2 rounded-lg md:hidden focus:ring-2 hover:bg-foreground focus:ring-primary-700 text-content"
         aria-controls="navbar-default"
         aria-expanded="false"
       >
@@ -32,13 +32,14 @@ export const NavMenu = () => {
         </svg>
       </button>
       <div
+        ref={ref}
         id="dropdown"
         className={`${
-          open ? 'hidden opacity-0' : 'opacity-100'
-        } transition-opacity ease-linear delay-150 md:hidden absolute p-2 right-2 top-14 z-10 w-32 bg-foreground rounded-lg shadow-md `}
+          isComponentVisible ? 'opacity-100' : 'hidden opacity-0'
+        } transition-opacity duration-200 ease-linear delay-150 md:hidden absolute p-2 right-2 top-16 z-10 w-32 bg-foreground rounded-lg shadow-md `}
       >
         <ul
-          className="text-sm text-gray-700 dark:text-gray-200 w-full"
+          className="text-base text-gray-700 dark:text-gray-200 w-full"
           aria-labelledby="dropdownDefault"
         >
           {links.map(({ name, path }) => (
---
+++ hooks/useComponentVisible.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react';
+
+export const useComponentVisible = (initialIsVisible: boolean) => {
+  const [isComponentVisible, setIsComponentVisible] =
+    useState(initialIsVisible);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: Event) => {
+    if (ref.current && !ref.current.contains(event.target as Node)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+};
