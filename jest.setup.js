6d6feed120dbefcbe01802f5711cb92013027459
EstivenCano <estivencano99@gmail.com>
2022-10-19 17:42:36 -0500

    [Add] useTheme testing and window mock

--- jest.setup.js
+++ jest.setup.js
@@ -10,3 +10,15 @@ jest.mock('next-i18next', () => ({
     };
   },
 }));
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
