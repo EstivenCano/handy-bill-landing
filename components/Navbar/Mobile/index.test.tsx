dd0e6450533fc207956c0df348aa84937b9c73f1
Estiven Cano <estivencano99@gmail.com>
2023-01-03 01:05:20 -0500

    [Update] Mobile Navbar test

--- components/Navbar/Mobile/index.test.tsx
+++ components/Navbar/Mobile/index.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { NavMenu } from '.';
 
@@ -12,11 +13,17 @@ describe('NavMenu functionality', () => {
     ).toBeVisible();
   });
 
-  test('Should render list of NavItems', () => {
+  test('Should render list of NavItems', async () => {
     render(<NavMenu />);
+
+    userEvent.click(
+      screen.getByRole('button', {
+        name: /main\-menu/i,
+      }),
+    );
 
     expect(screen.getByRole('list')).toBeVisible();
 
-    expect(screen.getAllByRole('link').length).toBe(5);
+    await waitFor(() => expect(screen.getAllByRole('link').length).toBe(5));
   });
 });
