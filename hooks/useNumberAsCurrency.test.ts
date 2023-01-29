cb2351e956af862f479a50e8172fb1ed0211e8b7
Estiven Cano <estivencano99@gmail.com>
2023-01-29 18:42:59 -0500

    [Add] Test for contact function

--- hooks/useNumberAsCurrency.test.ts
+++ hooks/useNumberAsCurrency.test.ts
@@ -2,6 +2,17 @@ import { renderHook } from '@testing-library/react';
 
 import { useNumberAsCurrency } from './useNumberAsCurrency';
 
+//Mock the Intl API to prevent errors during testing and return a fixed value
+jest.spyOn(Intl, 'DateTimeFormat').mockImplementation(() => {
+  return {
+    resolvedOptions: () => {
+      return {
+        locale: 'en-US',
+      };
+    },
+  } as unknown as Intl.DateTimeFormat;
+});
+
 describe('useNumberAsCurrency should handle currency', () => {
   test('It should return the formatted number', () => {
     const { result } = renderHook(() => useNumberAsCurrency());
